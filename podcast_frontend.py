f83583099d22d198dd14841750a8843e2ee169ef
Monica Hoang <109242823+MonicaHoang-29@users.noreply.github.com>
2023-08-19 11:16:19 -0500

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -42,7 +42,7 @@ def main():
 
         with col3:
             st.subheader("Podcast Guest")
-            st.write(podcast_info['guest_name'])
+            st.write(podcast_info['name'])
 
         with col4:
             st.subheader("Podcast Guest Details")
